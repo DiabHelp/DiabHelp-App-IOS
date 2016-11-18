45eb53663224ad7f30d649dcbfd93180f5e1bdf7
Sumbers <Sumbers@sumberss-mac.local>
2016-11-18 11:59:42 +0100

    Initial Commit

---
+++ Diabhelp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diabhelp
+//
+//  Created by Sumbers on 18/11/16.
+//  Copyright © 2016 Sumbers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
